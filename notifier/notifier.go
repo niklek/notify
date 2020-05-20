45961cce0d4fdb19b3a4949ee805a75158bcb050
niklek <nikitaleksikov@gmail.com>
2020-05-20 16:30:03 +0200

    feat: Implemented actual sending messages to url. Created custom http client.

--- notifier/notifier.go
+++ notifier/notifier.go
@@ -2,15 +2,26 @@
 package notifier
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"log"
+	"net"
+	"net/http"
+	"strconv"
 	"sync"
 	"time"
 )
 
 // Default number of workers for sending messages
 const MAX_WORKERS = 100
+
+// HTTP Request timeout
+const HTTP_REQUEST_TIMEOUT = 10
+
+// TCP timeout, TSL handshake timeout
+const HTTP_TRANSPORT_TIMEOUT = 5
 
 // Message represent a single message which will be send to a remote server
 type Message struct {
@@ -61,7 +72,7 @@ func (n *Notifier) Start() {
 	// Start cfg.NumWorkers workers
 	for i := 0; i < n.cfg.NumWorkers; i++ {
 		n.wg.Add(1)
-		go worker(n.ctx, i, n.q, n.wg)
+		go worker(n.ctx, i, n.q, n.cfg.Url, n.wg)
 	}
 
 	fmt.Println("[NOTIFIER] started", n.cfg.NumWorkers, "workers")
@@ -97,7 +108,7 @@ func (n *Notifier) Send(messages []Message) {
 }
 
 // Worker: reads from a q channel and sends a message
-func worker(ctx context.Context, i int, q <-chan Message, wg *sync.WaitGroup) {
+func worker(ctx context.Context, i int, q <-chan Message, url string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	// TODO: unsent messages can go to err channel
 	// Drain channel on cancel
@@ -106,6 +117,10 @@ func worker(ctx context.Context, i int, q <-chan Message, wg *sync.WaitGroup) {
 		}
 	}()
 
+	var err error
+	// create a HTTP client for sending all the notifications
+	client := newHTTPClient()
+
 	for m := range q {
 		select {
 		case <-ctx.Done():
@@ -113,10 +128,49 @@ func worker(ctx context.Context, i int, q <-chan Message, wg *sync.WaitGroup) {
 			fmt.Println("[WORKER", i, "] received [STOP] signal")
 			return
 		default:
-			// TODO: sending
-			time.Sleep(time.Second * 1)
-			fmt.Println("id:", i, "m:", m)
+			// Sending a notification
+			err = sendNotificationWithClient(client, url, m.Body)
+			if err != nil {
+				// TODO: Move the message into err channel
+				m.Err = err
+				log.Println("[WORKER", i, "] error:", err)
+				continue
+			}
+			fmt.Println("id:", i, "[SENT] m:", m)
 		}
 	}
 	fmt.Println("[WORKER", i, "] completed")
+}
+
+// Sends a notification via POST to url using HTTP client
+func sendNotificationWithClient(c *http.Client, url string, body string) error {
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(body)))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "text/plain")
+	resp, err := c.Do(req)
+	if err != nil {
+		return err
+	}
+	resp.Body.Close() // we do not need body
+
+	if resp.StatusCode != http.StatusOK {
+		return errors.New("Response status code:" + strconv.Itoa(resp.StatusCode))
+	}
+
+	return nil
+}
+
+// Creates a new custom HTTP client with timeouts: HTTP_TIMEOUT
+func newHTTPClient() *http.Client {
+	return &http.Client{
+		Timeout: time.Second * HTTP_REQUEST_TIMEOUT,
+		Transport: &http.Transport{
+			Dial: (&net.Dialer{
+				Timeout: HTTP_TRANSPORT_TIMEOUT * time.Second,
+			}).Dial,
+			TLSHandshakeTimeout: HTTP_TRANSPORT_TIMEOUT * time.Second,
+		},
+	}
 }
