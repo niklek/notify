0648028510901459ad3f85e569803d6153134d57
niklek <nikitaleksikov@gmail.com>
2020-05-21 16:07:49 +0200

    test: added tests for Notifier using httptest

---
+++ notifier/notifier_test.go
@@ -0,0 +1,125 @@
+package notifier
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+// Test basic Send using 2 messages
+// Test POST method on the target server
+// Test POST body (message content) on the target server
+// Test no failed messages after sending
+func TestSend(t *testing.T) {
+
+	const message = "test message"
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			t.Errorf("Expected ‘POST’ request method, got ‘%s’", r.Method)
+		}
+
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Errorf("Error while reading request body: %s", err)
+		}
+
+		s := string(body)
+		if s != message {
+			t.Errorf("Expected message %s received %s", message, s)
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer ts.Close()
+
+	// Init Notifier with the following settings
+	n := NewNotifier(Config{
+		Url:              ts.URL, // test server url
+		NumWorkers:       2,
+		SendingQueueSize: 2,
+		ErrorQueueSize:   2,
+	})
+	// Start workers
+	n.Start()
+	// Send message slice
+	n.Send([]Message{
+		Message{
+			Body: message,
+		},
+		Message{
+			Body: message,
+		},
+	})
+
+	qerr := n.ErrChan()
+loop:
+	for {
+		select {
+		case m := <-qerr:
+			t.Errorf("Unexpected message in the error channel: %s %s", m.Body, m.Err)
+		case <-time.After(time.Second):
+			break loop
+		}
+	}
+
+	// Complete Notifier
+	n.Stop()
+}
+
+// Test receiving failed messages when server is not available
+// Test failed messages body and non empty error
+func TestSendFails(t *testing.T) {
+
+	const message = "test message"
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusServiceUnavailable)
+	}))
+	defer ts.Close()
+
+	// Init Notifier with the following settings
+	n := NewNotifier(Config{
+		Url:              ts.URL, // test server url
+		NumWorkers:       2,
+		SendingQueueSize: 2,
+		ErrorQueueSize:   2,
+	})
+	// Start workers
+	n.Start()
+	// Send message slice
+	n.Send([]Message{
+		Message{
+			Body: message,
+		},
+		Message{
+			Body: message,
+		},
+	})
+
+	qerr := n.ErrChan()
+	i := 0
+loop:
+	for {
+		select {
+		case m := <-qerr:
+			i++
+			if m.Err == nil {
+				t.Errorf("Expected message with an error, received %s", m.Err)
+			}
+			if m.Body != message {
+				t.Errorf("Expected message with body %s, received %s", message, m.Body)
+			}
+		case <-time.After(time.Second):
+			break loop
+		}
+	}
+	if i != 2 {
+		t.Errorf("Expected number of failed messages %d received %d", 2, i)
+	}
+
+	// Complete Notifier
+	n.Stop()
+}
